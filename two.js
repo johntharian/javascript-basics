d5a4a0a5111a23544c4b2f1b6d15f9e6a7004a54
johntharian <jtharian13@gmail.com>
2020-02-02 23:19:51 +0530

    2

---
+++ two.js
@@ -0,0 +1,5 @@
+alert("Welcome to weight converter");
+var w=prompt("Enter your weight in pounds(lbs)");
+var x=w*0.454;
+alert("Your weight in kilograms is : " + x);
+console.log("Conversion Completed");
