2c2728202bb1ae5f2238e5a2eab2edb262d27a02
johntharian <jtharian13@gmail.com>
2020-02-04 20:01:56 +0530

    4

---
+++ four.js
@@ -0,0 +1,15 @@
+var x=0;
+while(x<5){
+    console.log("X is currently "+x);
+    x+=1;
+    if (x==3){
+        console.log("x=3")
+        break;
+    }
+}
+x=prompt("Enter a number")
+var i=0;
+while(i<10){
+    console.log(x*i);
+    i+=1;
+}
